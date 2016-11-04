fe614fe980a86d6c524acd7142947e5342ed86f2
Skovisen <christianskovborg@outlook.com>
2016-11-04 14:04:30 +0100

    This is the first commit, to publish the first parts of the project, to take the first steps of this journey. Wish me luck!

---
+++ src/chapter_code/hello/Hello.java
@@ -0,0 +1,11 @@
+package chapter_code.hello;
+
+/**
+ * Created by chris on 04.11.2016.
+ */
+public class Hello {
+    public static void main(String[] args) {
+// generate some simple output
+        System.out.println("Hello, World!");
+    }
+}
