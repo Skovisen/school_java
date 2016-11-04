29e0c96c718a5dd37bb1cf665e7be5385ea9d51f
Skovisen <christianskovborg@outlook.com>
2016-11-04 14:21:35 +0100

    Updated Hello.java to include two sentences on two separate lines in one statement.

--- src/chapter_code/hello/Hello.java
+++ src/chapter_code/hello/Hello.java
@@ -3,7 +3,6 @@ package chapter_code.hello;
 public class Hello {
     public static void main(String[] args) {
     // generate some simple output
-        System.out.println("Hello, World!");    //First line
-        System.out.println("How are you?");     //Another line
+        System.out.println("Hello!\n How are you doing?\n");    //First line
     }
 }
