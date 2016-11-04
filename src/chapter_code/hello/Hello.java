aa21739fcca12054d963eca29a588d8b74b1d574
Skovisen <christianskovborg@outlook.com>
2016-11-04 14:11:24 +0100

    Updated Hello.java to include two print statements rather than one.

--- src/chapter_code/hello/Hello.java
+++ src/chapter_code/hello/Hello.java
@@ -1,11 +1,9 @@
 package chapter_code.hello;
 
-/**
- * Created by chris on 04.11.2016.
- */
 public class Hello {
     public static void main(String[] args) {
-// generate some simple output
-        System.out.println("Hello, World!");
+    // generate some simple output
+        System.out.println("Hello, World!");    //First line
+        System.out.println("How are you?");     //Another line
     }
 }
