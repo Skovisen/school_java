300f69560174635175e0923a5826de4b01da051e
Skovisen <christianskovborg@outlook.com>
2016-11-05 18:23:44 +0100

    Did some exercises (2.2, 2.3) and rearranged code in Hello.java

---
+++ src/chapter_code/ex_2_3/Time.java
@@ -0,0 +1,31 @@
+package chapter_code.ex_2_3;
+
+public class Time {
+    public static void main(String[] args) {
+        // generate some simple output
+        double day = 24;
+        int hour = 18;
+        int minute = 3;
+        int seconds = 59;
+        double sec_now = ((hour*3600)+(minute * 60)+seconds);
+        double percent = ((sec_now*100)/(day*3600));
+        System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        if (minute <= 9) {
+            System.out.print("0"+minute);
+        }
+        else{
+            System.out.print(+minute);
+        }
+
+        System.out.print(":");
+        System.out.print(seconds);
+        System.out.println(".");
+        System.out.print("Number of seconds since midnight: ");
+        System.out.println(sec_now);
+        System.out.print("Percentage passed of the day: ");
+        System.out.println(percent);
+    }
+
+}
--- src/chapter_code/hello/Hello.java
+++ src/chapter_code/hello/Hello.java
@@ -3,6 +3,7 @@ package chapter_code.hello;
 public class Hello {
     public static void main(String[] args) {
     // generate some simple output
-        System.out.println("Hello!\n How are you doing?\n");    //First line
+        System.out.println("Hello!\nHow are you doing?\n");    //First line
+        System.out.println("She said \"Hello!\" to me."); //Second line
     }
 }
