c8464764c89d222b81d907eda2bad3095f6d91ae
Skovisen <christianskovborg@outlook.com>
2016-12-13 15:45:06 +0100

    Added new dir "usingjava", which is another learning framework I use

---
+++ src/usingjava/ex_3_2_2/solve.java
@@ -0,0 +1,15 @@
+package usingjava.ex_3_2_2;
+
+import textio.TextIO;
+
+/**
+ * Created by chris on 13.12.2016.
+ */
+public class solve {
+    public static void main(String[] args){
+        int n = TextIO.getlnInt();
+    }
+        while (int n != 1){
+            
+    }
+}
