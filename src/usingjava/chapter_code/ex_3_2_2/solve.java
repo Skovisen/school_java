d9072cc217b1665dc6960f9c6a209f2205062c78
Skovisen <christianskovborg@outlook.com>
2016-12-17 14:25:23 +0100

    Finished up solve.java, cleaned up file structure. Merging this with master

---
+++ src/usingjava/chapter_code/ex_3_2_2/solve.java
@@ -0,0 +1,48 @@
+package usingjava.chapter_code.ex_3_2_2;
+
+import textio.TextIO;
+
+public class solve {
+    public static void main(String[] args) {
+        //main, calls the method getNumber()
+        getNumber();
+    }
+
+    //getNumber(), does all the work
+    private static void getNumber() {
+
+        //Ask user for a postive integer
+        System.out.print("Please enter a positive number for assessment:\n");
+        int n = TextIO.getlnInt();
+
+        //If integer wasn't positive, prompt user again, by calling getNumber() recursively
+        if (n < 1) {
+            System.out.println("Woops, that was not a positive integer!");
+            getNumber();
+        }
+
+        //If everything's good so far, we repeat until we reach n = 1
+        else {
+            while (n != 1) {
+                    //While we repeat, we check for two conditions, is n even?
+                    if (n % 2 == 0) {
+
+                        n = n/2;
+
+                    }
+
+                    //If not, then n has to be odd:
+
+                    else {
+
+                        n = (3 * n) + 1;
+
+                    }
+
+                    //And when we've ended one cycle, we print out the current value of n
+                    System.out.println(n);
+
+                    }
+            }
+    }
+}
