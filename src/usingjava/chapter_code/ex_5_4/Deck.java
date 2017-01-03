52620a62b880931d2734479e6596244dbeb2edef
Skovisen <christianskovborg@outlook.com>
2017-01-03 20:01:40 +0100

    Updated a whole bunch of stuff, more coming.

---
+++ src/usingjava/chapter_code/ex_5_4/Deck.java
@@ -0,0 +1,25 @@
+package usingjava.chapter_code.ex_5_4;
+
+public class Deck {
+
+    //Constructor
+    public Deck(){
+
+    }
+
+    //Gathers all cards and shuffles them
+    public void shuffle(){
+
+    }
+
+    //Deals one card and returns "Card"
+    public Card dealCard(){
+
+    }
+
+    //Returns how many cards are left in the deck
+    public int cardsLeft(){
+
+    }
+}
+
---
+++ src/usingjava/chapter_code/ex_5_4/Hand.java
@@ -0,0 +1,33 @@
+package usingjava.chapter_code.ex_5_4;
+
+public class Hand {
+
+    public Hand(){
+
+    }
+    public void addCard(Card c){
+
+    }
+    public void removeCard(Card c){
+
+    }
+    public void removeCard(int pos){
+
+    }
+    public void clear(){
+
+    }
+    public int getCardCount(){
+
+    }
+    public Card getCard(int pos){
+
+    }
+    public void sortBySuit{
+
+    }
+    public void sortByValue{
+
+    }
+
+}
